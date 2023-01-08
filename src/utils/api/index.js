394df050f1a48dd2bef5aa15dac16ad17d4aefab
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-01-08 17:08:42 -0500

    There was a bug causing advice to be shown multiple times in a row if the requests are too close together so I added the no cache config option to the request

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,6 +1,8 @@
 const api = (() => {
   async function getRandomAdvice() {
-    const response = await fetch("https://api.adviceslip.com/advice");
+    const response = await fetch("https://api.adviceslip.com/advice", {
+      cache: "no-cache",
+    });
     const json = await response.json();
     return json.slip;
   }
